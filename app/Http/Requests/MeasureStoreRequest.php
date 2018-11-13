be18dc1f26b8d7605f7cbdd13cda581d9841f5ec
CristhianChaucaMariaca <cristhiancchm@gmail.com>
2018-11-13 18:23:11 -0400

    se comienza a validar los campos de los formularios restantes

--- app/Http/Requests/MeasureStoreRequest.php
+++ app/Http/Requests/MeasureStoreRequest.php
@@ -4,7 +4,7 @@ namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class TypeStoreRequest extends FormRequest
+class MeasureStoreRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
@@ -24,7 +24,8 @@ class TypeStoreRequest extends FormRequest
     public function rules()
     {
         return [
-            'name'=>'string|required|unique:types,name',
+            'name'=>'string|required',
+            'no'=>'alfa|required',
         ];
     }
 }
