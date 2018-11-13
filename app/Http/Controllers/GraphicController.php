77d236d445dcf02743a9c39a656f9c768717db12
CristhianChaucaMariaca <cristhiancchm+git@gmail.com>
2018-11-13 05:02:17 -0400

    Ser actualizo los datos graficos con nuevos datos y se cambio el tipo de grafico a column, con labels de fechas de creación

--- app/Http/Controllers/GraphicController.php
+++ app/Http/Controllers/GraphicController.php
@@ -7,6 +7,7 @@ use App\Sale;
 use App\Kardex;
 use App\Charts\Buys;
 use Illuminate\Support\Facades\DB;
+use Carbon\Carbon;
 
 use Illuminate\Http\Request;
 
@@ -16,21 +17,43 @@ class GraphicController extends Controller
 	public function stoksgraphics(){
     	$kardex=Kardex::orderBy('id','ASC')->pluck('balance');
     	$value=Kardex::orderBy('id','ASC')->pluck('value');
-    	//$buysc=Kardex::orderBy('id','ASC')->pluck('created_at');
+    	$date=DB::table('kardexes')->select('created_at')
+            ->orderBy('id','ASC')
+            ->get();
+        $dat=array();
+        foreach ($date as $d) {
+            $dat[]= $d->created_at;
+        }
     	$chart= new Buys;
     	$chart->loaderColor('red');
     	$chart->title('Grafica de Stock en el registro');
-    	$chart->dataset('Promedio Ponderado','line',$value)->options(['color'=>'red']);
-    	$chart->dataset('Stock','line',$kardex);
+        $chart->labels($dat);
+        $chart->dataset('Stock','column',$kardex);
+    	$chart->dataset('Precio total','column',$value)->options(['color'=>'red']);
     	return view('admin.graphics.buys',compact('chart'));
     }
     public function buysgraphics(){
     	$buys=Buy::orderBy('id','ASC')->pluck('cuantity');
-    	//$buysc=Buy::orderBy('id','ASC')->pluck('created_at');
+    	$dates=DB::table('buys')->select('created_at')
+            ->orderBy('id','ASC')
+            ->get();
+        $dat= array();
+        foreach ($dates as $d) {
+            $dat[]=$d->created_at;
+        }
+        $values=DB::table('buys')->select('unitary','cuantity')
+            ->orderBy('id','ASC')
+            ->get();
+        $val=array();
+        foreach ($values as $v) {
+            $val[]=$v->unitary*$v->cuantity;
+        }
     	$chart= new Buys;
     	$chart->loaderColor('red');
+        $chart->labels($dat);
     	$chart->title('Grafica de compras totales');
-    	$chart->dataset('Compra','line',$buys);
+        $chart->dataset('Cantidad','column',$buys);
+    	$chart->dataset('Valor','column',$val)->options(['color'=>'blue']);
     	return view('admin.graphics.buys',compact('chart'));
     }
     public function salesgraphics(){
@@ -50,12 +73,20 @@ class GraphicController extends Controller
         foreach ($valor as $v) {
             $val[]= $v->cuantity*$v->unitary;
         }
+        $date=DB::table('sales')->select('created_at')
+            ->orderBy('id','ASC')
+            ->get();
+        $dat=array();
+        foreach ($date as $d) {
+            $dat[]= $d->created_at;
+        }
     	$chart= new Buys;
     	$chart->loaderColor('red');
     	$chart->title('Grafica de exportaciones totales');
-    	$chart->dataset('Exportacion','line',$salesc);
-        $chart->dataset('Ganancia','line',$sal)->options(['color'=>'blue']);
-    	$chart->dataset('Precio','line',$val)->options(['color'=>'red']);
+        $chart->labels($dat);
+    	$chart->dataset('Cantidad','column',$salesc);
+        $chart->dataset('Precio','column',$val)->options(['color'=>'blue']);
+        $chart->dataset('Ganancia','column',$sal)->options(['color'=>'red']);
     	return view('admin.graphics.sales',compact('chart'));
     }
 }
